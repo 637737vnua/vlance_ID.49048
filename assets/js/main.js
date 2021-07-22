790e7750ca8ccec76a3be0adac351aa9b28de1bc
duykhanh <zerok49tt@gmail.com>
2021-07-23 06:39:38 +0700

    change copy number bank

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,30 @@
 (function(){
-    const text = document.querySelector(`#number-bank`).innerHTML;
-    const btn = document.querySelector('.number-bank .btn-copy');
-    const tab = document.querySelector('.number-bank');
-    const icon = document.querySelector('#img-copy');
-
-    function copy() {
-      const input =     document.createElement('input');
-      input.value = text;
-      tab.appendChild(input);
-      input.focus();
-      input.select();
-      document.execCommand("copy");
-      tab.removeChild(input);
-      icon.src = 'https://hoclammypham.com.vn/wp-content/uploads/icon-check-2.png';
-      setTimeout(()=>{
-        icon.src = './assets/images/icon_coppy.png'
-      }, 3000);
+    function copyToClipboard(text, icon) {
+      if (window.clipboardData && window.clipboardData.setData) {
+          return clipboardData.setData("Text", text); 
+  
+      } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+          let textarea = document.createElement("textarea");
+          textarea.textContent = text;
+          document.body.appendChild(textarea);
+          textarea.select();
+          try {
+              icon.src = 'https://hoclammypham.com.vn/wp-content/uploads/icon-check-2.png';
+              setTimeout(()=>{
+                icon.src = './assets/images/icon_coppy.png'
+              }, 2000);
+              return document.execCommand("copy");
+          } catch (ex) {
+              return false;
+          } finally {
+              document.body.removeChild(textarea);
+          }
+      }
     }
 
-    btn.onclick = () => {
-        copy()
-    }
+    $('.number-bank .btn-copy').click(function(){
+      const icon = this.lastElementChild;
+      const text = this.parentElement.innerText.split(' ');
+      copyToClipboard(text[1], icon);
+    });
 })()
